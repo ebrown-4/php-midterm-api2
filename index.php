fa2287ac0b58230af113937c1a34b3cc2b670736
Ericka Brown <grimesericka@gmail.com>
2026-04-11 23:18:09 -0500

    Fix root index.php to prevent extra JSON output

--- index.php
+++ index.php
@@ -10,26 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-// Simple root response for the tester
-echo json_encode([
-    "message" => "API root working",
-    "endpoints" => [
-        "/api/authors/read.php",
-        "/api/authors/read_single.php?id=1",
-        "/api/authors/create.php",
-        "/api/authors/update.php",
-        "/api/authors/delete.php",
+// DO NOT OUTPUT ANYTHING HERE
+// The tester will break if this file prints JSON
 
-        "/api/categories/read.php",
-        "/api/categories/read_single.php?id=1",
-        "/api/categories/create.php",
-        "/api/categories/update.php",
-        "/api/categories/delete.php",
-
-        "/api/quotes/read.php",
-        "/api/quotes/read_single.php?id=1",
-        "/api/quotes/create.php",
-        "/api/quotes/update.php",
-        "/api/quotes/delete.php"
-    ]
-]);
+echo json_encode(["message" => "API root working"]);
