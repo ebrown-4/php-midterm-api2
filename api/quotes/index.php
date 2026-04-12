bcc01a22b5470cf515d9288ea5eb5c5f6ae05ad7
Ericka Brown <grimesericka@gmail.com>
2026-04-12 00:04:27 -0500

    Suppress warnings

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
