a6abe1b42c6d7d7223f112d55237ef500534a9d5
Ericka Brown <grimesericka@gmail.com>
2026-04-11 20:51:02 -0500

    Fix quotes index to return IDs

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,38 +1,15 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once('../../config/Database.php');
-include_once('../../models/Quotes.php');
+include_once '../../config/Database.php';
+include_once '../../models/Quotes.php';
 
 $database = new Database();
 $db = $database->connect();
 
 $quotes = new Quotes($db);
-
-// If ID is provided, return a single quote
-if (isset($_GET['id'])) {
-    $quotes->id = $_GET['id'];
-
-    $result = $quotes->read_single();
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-
-    if ($row) {
-        echo json_encode([
-            "id" => $row['id'],
-            "quote" => $row['quote'],
-            "author" => $row['author'],
-            "category" => $row['category']
-        ]);
-    } else {
-        echo json_encode(["message" => "quote_id Not Found"]);
-    }
-
-    exit;
-}
-
-// Otherwise return all quotes
 $result = $quotes->read();
+
 $num = $result->rowCount();
 
 if ($num > 0) {
@@ -42,8 +19,8 @@ if ($num > 0) {
         $quotes_arr[] = [
             "id" => $row['id'],
             "quote" => $row['quote'],
-            "author" => $row['author'],
-            "category" => $row['category']
+            "author_id" => $row['author_id'],
+            "category_id" => $row['category_id']
         ];
     }
 
